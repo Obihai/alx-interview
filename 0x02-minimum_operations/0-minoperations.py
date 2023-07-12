c4afe3659b1e2fed11be461d411ff4c1d2055854
Kaegan Davis Haidome <obichi@coseo.com>
2023-07-12 16:22:12 +0100

    Fixed PEP8 error

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ Module for 0-minoperations"""
 
+
 def minOperations(n):
     """
-    Finds the minimum number of operations required to 
+    Finds the minimum number of operations required to
     transform a positive integer `n` into 1.
 
     Parameters:
@@ -14,15 +15,15 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while factor <= n:
         if n % factor == 0:
             n //= factor
             operations += factor
         else:
             factor += 1
-    
+
     return operations
