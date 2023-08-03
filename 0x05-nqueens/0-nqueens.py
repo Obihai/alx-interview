d84e4ee8169afe98055128d4405c752ca9e9fdc7
Obihai <obichi@coseo.com>
2023-08-03 07:45:36 +0100

    Nqueens solved using backtracking

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,60 +1,56 @@
 #!/usr/bin/python3
-""" Nqueens """
-
+""" N queens """
 
 import sys
 
 
-def is_safe(board, row, col):
-    """
-    Check if a queen can be placed at the given row and col
-    without attacking any other queens on the board.
-    """
-    for i in range(row):
-        if board[i] == col or abs(board[i] - col) == abs(i - row):
-            return False
-    return True
+def usage():
+    """ Print usage information and exit """
+    print("Usage: nqueens N")
+    sys.exit(1)
 
 
-def solve_nqueens(board, row, N):
-    """
-    Solve the N Queens problem using backtracking.
-    """
-    if row == N:
-        print([[i, board[i]] for i in range(N)])
-        return
-
-    # Try placing a queen in each column of the current row.
-    for col in range(N):
-        if is_safe(board, row, col):
-            board[row] = col
-            solve_nqueens(board, row + 1, N)
-            board[row] = -1  # Backtrack to find other solutions.
-
-
-def nqueens(N):
-    """
-    Solve the N Queens problem and print all possible solutions.
-    """
-    board = [-1] * N
-
-    # Start the backtracking algorithm from the first row (row 0).
-    solve_nqueens(board, 0, N)
-
-
-if __name__ == "__main__":
+def check_input():
+    """ Check command-line arguments for validity """
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+        usage()
 
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
+    n = sys.argv[1]
+    if not n.isdigit():
         print("N must be a number")
         sys.exit(1)
 
-    if N < 4:
+    n = int(n)
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    nqueens(N)
+    return n
+
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ Find possible positions for the queens on the chessboard """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """ Solve the N Queens problem and print all possible solutions """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+if __name__ == "__main__":
+    n = check_input()
+    solve(n)
